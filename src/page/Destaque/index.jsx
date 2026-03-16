1d77f25a46c97539d3656e2918f91ca06c00a675
Gabriel Fagundes <gabrielfagundesvv@gmail.com>
2026-03-16 12:53:50 -0300

    foto modificadas e adicionado mais um slide

--- src/page/Destaque/index.jsx
+++ src/page/Destaque/index.jsx
@@ -1,57 +1,65 @@
-import React from 'react';
-import './style.css'
-import Button from '../../components/Button';
-import { useNavigate } from 'react-router-dom';
-const Destaque = () => {
+import React from "react";
+import "./style.css";
+import Button from "../../components/Button";
+import { useNavigate } from "react-router-dom";
 
+const Destaque = () => {
+  // Usei os nomes (name, category, image_url) para padronizar com seu Backend
   const produtos = [
     {
       id: 1,
-      nome: "Burger Clássico",
-      categoria: "Hambúrguer",
-      img: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd"
+      name: "Ninja Smash Duplo",
+      category: "Hambúrgueres",
+      image_url:
+        "https://images.unsplash.com/photo-1594212699903-ec8a3eca50f5?q=80&w=500&auto=format&fit=crop",
     },
     {
       id: 2,
-      nome: "Cheddar Bacon",
-      categoria: "Bebidas",
-      img: "https://images.unsplash.com/photo-1550547660-d9450f859349"
+      name: "Coca-Cola Gelada",
+      category: "Bebidas",
+      image_url:
+        "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?q=80&w=500&auto=format&fit=crop",
     },
     {
-      id: 3,
-      nome: "Burger Especial",
-      categoria: "Complementos",
-      img: "https://images.unsplash.com/photo-1606755962773-d324e2d53c72"
-    }
+  id: 3,
+  name: "Batata Frita Ninja",
+  category: "Acompanhamentos",
+  image_url: "https://images.pexels.com/photos/1893556/pexels-photo-1893556.jpeg?auto=compress&cs=tinysrgb&w=500"
+},
+    {
+      id: 4,
+      name: "Combo Shogun",
+      category: "Combo",
+      image_url:
+        "https://images.unsplash.com/photo-1610614819513-58e34989848b?q=80&w=500&auto=format&fit=crop",
+    },
   ];
 
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
   return (
-    <section className="menu-destaque" id='menu'>
+    <section className="menu-destaque" id="menu">
       <div className="menu-header">
-        <h2 className='tag'> Destaques do Cardápio</h2>
+        <h2 className="tag">Destaques do Cardápio</h2>
         <p>Os burgers mais pedidos da casa</p>
       </div>
 
       <div className="menu-grid">
         {produtos.map((item) => (
           <div className="menu-card" key={item.id}>
-
-            <img src={item.img} alt={item.nome} />
+            <img src={item.image_url} alt={item.name} />
 
             <div className="overlay">
-              <h3>{item.categoria}</h3>
-               <Button variant="primary" onClick={() => navigate("/cardapio") }>
-    Ir para Cardápio
-  </Button>
+              <h3>{item.category}</h3>
+              <Button variant="primary" onClick={() => navigate("/cardapio")}>
+                Ir para Cardápio
+              </Button>
             </div>
-
           </div>
         ))}
       </div>
     </section>
   );
-}
+};
 
 export default Destaque;
